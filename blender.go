953f159b0197d2d22472445cab6086f84f82c970
Albert Zhang <zhgwenming@gmail.com>
2014-10-17 11:10:03 +0800

    bridge docker in blender

--- blender.go
+++ blender.go
@@ -3,11 +3,16 @@ package main
 import (
 	"github.com/docker/docker/reexec"
 	_ "github.com/zhgwenming/blender/docker"
+	"os"
 )
 
 func main() {
+	// bridge "blender docker" to docker
+	if len(os.Args) > 1 && os.Args[1] == "docker" {
+		os.Args = os.Args[1:]
+	}
+
 	if reexec.Init() {
 		return
 	}
-	//dockercli.Main()
 }
