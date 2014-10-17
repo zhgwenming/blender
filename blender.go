a0411f6ed843e1f1e7398f89a76d44892143c3f2
Albert Zhang <zhgwenming@gmail.com>
2014-10-17 22:11:46 +0800

    pretend that we're static

--- blender.go
+++ blender.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	version "github.com/docker/docker/dockerversion"
 	"github.com/docker/docker/reexec"
 	_ "github.com/zhgwenming/blender/docker"
 	"os"
 )
 
 func main() {
+	version.IAMSTATIC = true
+
 	// bridge "blender docker" to docker
 	if len(os.Args) > 1 && os.Args[1] == "docker" {
 		os.Args = os.Args[1:]
